692cee91dadf6cf9243773f42b51bedaebcce473
kohanss <muhfarkhan05@gmail.com>
2024-06-11 09:17:46 +0700

    menambahkan index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>git remot</title>
+</head>
+<body>
+    <h1>ssfgsfdgjsfdbhjsfv</h1>
+</body>
+</html>
